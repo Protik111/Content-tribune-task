8b6e59348a431b9ab4527709165388cb322e4884
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2022-06-19 20:30:56 +0600

    client and concurrent setup added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container-fluid p-0">
+      <div className="d-flex justify-content-center mt-5">
+        <h1>Hi, There</h1>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button type="button" class="btn btn-info">View Course</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
