94a6274f9cf39d884319a467d2bb4fcccea32148
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2022-06-21 07:52:58 +0600

    ready for deploy

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.use('/api/user', require('../routes/user'));
 router.use('/api/user', require('../routes/auth'));
 router.use('/api/course', require('../routes/course'));
+
+if (process.env.NODE_ENV === 'production') {
+    router.use(express.static(path.join(__dirname, '../client/build')))
+  
+    router.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    )
+  } else {
+    router.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 //health route
 router.get('/health', (_req, res) => {
